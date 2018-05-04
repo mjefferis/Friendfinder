dbf9f7721e376698a6ff59bced3b16bf1608cb0d
Matt <mjosephjefferis@gmail.com>
2018-05-04 14:07:08 -0400

    everything works besides modal

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendArray = [
+    {
+      name: "Bob",
+      pic: "https://www.google.com/search?q=google+search&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjW_8KEjNvaAhXBqFkKHTqaAD4Q_AUIDSgE&biw=1085&bih=564&dpr=2#imgrc=qoXTelFBS1Ji7M:",
+      scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+    }
+  ];
+  
+  module.exports = friendArray;
+  
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+
+//whytf does this not work?
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+//require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
